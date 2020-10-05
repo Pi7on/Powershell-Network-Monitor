3d22f705195c883ae6e80363a2acab76bdecae04
Pi7on <45372614+Pi7on@users.noreply.github.com>
2020-10-05 22:20:32 +0200

    update to V2.0
    - switch from pre-formatted data to raw data to improve refresh speed
    - divide displayed speed value by refresh/1000 to improve accuracy when refresh != 1 second
    - included version number

--- psnetmon.ps1
+++ psnetmon.ps1
@@ -1,31 +1,32 @@
 #Author: https://github.com/Pi7on
+#Version: 2.0
 
 param ($unit, $refresh)
 
 switch ($unit) {
-	"b" {$multiplier = 8}
-	"Kb" {$multiplier = 8 * [Math]::Pow(10,-3)}
-	"Mb" {$multiplier = 8 * [Math]::Pow(10,-6)}
-	"Gb" {$multiplier = 8 * [Math]::Pow(10,-9)}
-	"Tb" {$multiplier = 8 * [Math]::Pow(10,-12)}
-	"Pb" {$multiplier = 8 * [Math]::Pow(10,-15)}
-	"Eb" {$multiplier = 8 * [Math]::Pow(10,-18)}
-	"Zb" {$multiplier = 8 * [Math]::Pow(10,-21)}
-	"Yb" {$multiplier = 8 * [Math]::Pow(10,-24)}
+	"b" {$unit_multiplier = 8}
+	"Kb" {$unit_multiplier = 8 * [Math]::Pow(10,-3)}
+	"Mb" {$unit_multiplier = 8 * [Math]::Pow(10,-6)}
+	"Gb" {$unit_multiplier = 8 * [Math]::Pow(10,-9)}
+	"Tb" {$unit_multiplier = 8 * [Math]::Pow(10,-12)}
+	"Pb" {$unit_multiplier = 8 * [Math]::Pow(10,-15)}
+	"Eb" {$unit_multiplier = 8 * [Math]::Pow(10,-18)}
+	"Zb" {$unit_multiplier = 8 * [Math]::Pow(10,-21)}
+	"Yb" {$unit_multiplier = 8 * [Math]::Pow(10,-24)}
 	
-	"B" {$multiplier = 1}
-	"KB" {$multiplier = [Math]::Pow(10,-3)}
-	"MB" {$multiplier = [Math]::Pow(10,-6)}
-	"GB" {$multiplier = [Math]::Pow(10,-9)}
-	"TB" {$multiplier = [Math]::Pow(10,-12)}
-	"PB" {$multiplier = [Math]::Pow(10,-15)}
-	"EB" {$multiplier = [Math]::Pow(10,-18)}
-	"ZB" {$multiplier = [Math]::Pow(10,-21)}
-	"YB" {$multiplier = [Math]::Pow(10,-24)}
+	"B" {$unit_multiplier = 1}
+	"KB" {$unit_multiplier = [Math]::Pow(10,-3)}
+	"MB" {$unit_multiplier = [Math]::Pow(10,-6)}
+	"GB" {$unit_multiplier = [Math]::Pow(10,-9)}
+	"TB" {$unit_multiplier = [Math]::Pow(10,-12)}
+	"PB" {$unit_multiplier = [Math]::Pow(10,-15)}
+	"EB" {$unit_multiplier = [Math]::Pow(10,-18)}
+	"ZB" {$unit_multiplier = [Math]::Pow(10,-21)}
+	"YB" {$unit_multiplier = [Math]::Pow(10,-24)}
 	
 	default {
 		$unit = "Mbit"
-		$multiplier = $multiplier = 8 * [Math]::Pow(10,-6)
+		$unit_multiplier = $unit_multiplier = 8 * [Math]::Pow(10,-6)
 	}
 }
 
@@ -34,15 +35,28 @@ if (-not $refresh) {
 }
 
 while(1) {
-	$interface = Get-CimInstance -class Win32_PerfFormattedData_Tcpip_NetworkInterface | select BytesReceivedPersec, BytesSentPersec, Name
+	$interface = Get-CimInstance -class Win32_PerfRawData_Tcpip_NetworkInterface | select BytesReceivedPersec, BytesSentPersec, Name
 	
-	$DL_speed = $interface.BytesReceivedPersec
-	$UL_speed = $interface.BytesSentPersec
+	$values_current = ($interface.BytesReceivedPersec, $interface.BytesSentPersec)
 	
-	cls	
-	#Write-Host "$([char]0x2191)U:"($UL_speed * $multiplier)$unit"/s`n$([char]0x2193)D:"($DL_speed * $multiplier)$unit"/s"
-	Write-Host $interface.name
-	Write-Host "U$([char]0x2b06)" $unit"/s" ($UL_speed * $multiplier)"`nD$([char]0x2b07)"$unit"/s"($DL_speed * $multiplier)
+	if($values_prev -ne $null){
 	
+		$UL_speed = $values_current[1] - $values_prev[1]
+		$DL_speed = $values_current[0] - $values_prev[0]
+		
+		
+		$UL_display_value = ($UL_speed * $unit_multiplier / ($refresh/1000))
+		$DL_display_value = ($DL_speed * $unit_multiplier / ($refresh/1000))
+		
+		cls	
+		Write-Host $interface.name
+		Write-Host "U$([char]0x2b06)"$unit"/s "$UL_display_value"`nD$([char]0x2b07) "$unit"/s "$DL_display_value
+		#Write-Host "$([char]0x2191)U:"($UL_speed * $unit_multiplier)$unit"/s`n$([char]0x2193)D:"($DL_speed * $unit_multiplier)$unit"/s"
+		
+	}
+	$values_prev = $values_current.Clone()
 	Start-Sleep -milliseconds $refresh
+	#!!!
+	#se refresho una volta ogni 2 secondi, la velocità che vedo non è più in bytes/secondo ma in bytes/2 secondi, quindi devo dimezzare
+	#il valore per ottenere una velocità più o meno accurata in bytes/secondo
 }
