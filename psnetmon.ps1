d4f9c9e17187dc279d22209523dcf59b3df99b30
Pi7on <45372614+Pi7on@users.noreply.github.com>
2020-10-05 16:47:05 +0200

    Correct typo in variable name

--- psnetmon.ps1
+++ psnetmon.ps1
@@ -3,29 +3,29 @@
 param ($unit, $refresh)
 
 switch ($unit) {
-	"b" {$multiplyer = 8}
-	"Kb" {$multiplyer = 8 * [Math]::Pow(10,-3)}
-	"Mb" {$multiplyer = 8 * [Math]::Pow(10,-6)}
-	"Gb" {$multiplyer = 8 * [Math]::Pow(10,-9)}
-	"Tb" {$multiplyer = 8 * [Math]::Pow(10,-12)}
-	"Pb" {$multiplyer = 8 * [Math]::Pow(10,-15)}
-	"Eb" {$multiplyer = 8 * [Math]::Pow(10,-18)}
-	"Zb" {$multiplyer = 8 * [Math]::Pow(10,-21)}
-	"Yb" {$multiplyer = 8 * [Math]::Pow(10,-24)}
+	"b" {$multiplier = 8}
+	"Kb" {$multiplier = 8 * [Math]::Pow(10,-3)}
+	"Mb" {$multiplier = 8 * [Math]::Pow(10,-6)}
+	"Gb" {$multiplier = 8 * [Math]::Pow(10,-9)}
+	"Tb" {$multiplier = 8 * [Math]::Pow(10,-12)}
+	"Pb" {$multiplier = 8 * [Math]::Pow(10,-15)}
+	"Eb" {$multiplier = 8 * [Math]::Pow(10,-18)}
+	"Zb" {$multiplier = 8 * [Math]::Pow(10,-21)}
+	"Yb" {$multiplier = 8 * [Math]::Pow(10,-24)}
 	
-	"B" {$multiplyer = 1}
-	"KB" {$multiplyer = [Math]::Pow(10,-3)}
-	"MB" {$multiplyer = [Math]::Pow(10,-6)}
-	"GB" {$multiplyer = [Math]::Pow(10,-9)}
-	"TB" {$multiplyer = [Math]::Pow(10,-12)}
-	"PB" {$multiplyer = [Math]::Pow(10,-15)}
-	"EB" {$multiplyer = [Math]::Pow(10,-18)}
-	"ZB" {$multiplyer = [Math]::Pow(10,-21)}
-	"YB" {$multiplyer = [Math]::Pow(10,-24)}
+	"B" {$multiplier = 1}
+	"KB" {$multiplier = [Math]::Pow(10,-3)}
+	"MB" {$multiplier = [Math]::Pow(10,-6)}
+	"GB" {$multiplier = [Math]::Pow(10,-9)}
+	"TB" {$multiplier = [Math]::Pow(10,-12)}
+	"PB" {$multiplier = [Math]::Pow(10,-15)}
+	"EB" {$multiplier = [Math]::Pow(10,-18)}
+	"ZB" {$multiplier = [Math]::Pow(10,-21)}
+	"YB" {$multiplier = [Math]::Pow(10,-24)}
 	
 	default {
 		$unit = "Mbit"
-		$multiplyer = $multiplyer = 8 * [Math]::Pow(10,-6)
+		$multiplier = $multiplier = 8 * [Math]::Pow(10,-6)
 	}
 }
 
@@ -40,8 +40,8 @@ while(1) {
 	$UL_speed = $interface.BytesSentPersec
 	
 	cls	
-	#Write-Host "$([char]0x2191)U:"($UL_speed * $multiplyer)$unit"/s`n$([char]0x2193)D:"($DL_speed * $multiplyer)$unit"/s"	
-	Write-Host "U$([char]0x2b06)" $unit"/s" ($UL_speed * $multiplyer)"`nD$([char]0x2b07)"$unit"/s"($DL_speed * $multiplyer)
+	#Write-Host "$([char]0x2191)U:"($UL_speed * $multiplier)$unit"/s`n$([char]0x2193)D:"($DL_speed * $multiplier)$unit"/s"	
+	Write-Host "U$([char]0x2b06)" $unit"/s" ($UL_speed * $multiplier)"`nD$([char]0x2b07)"$unit"/s"($DL_speed * $multiplier)
 	
 	Start-Sleep -milliseconds $refresh
 }
